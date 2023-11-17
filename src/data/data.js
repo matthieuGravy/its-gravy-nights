19131c0443c96c64c393ef48a9a6de23900a79bf
matthieuGravy <matthieugravy@gmail.com>
2023-11-18 00:00:00 +0100

    menu

---
+++ src/data/data.js
@@ -0,0 +1,43 @@
+const liste = [
+  {
+    name: "Shrimp Tomato & Basil",
+    content:
+      "Indulge in the exquisite flavors of our Shrimp Tomato & Basil dish. Succulent shrimp, ripe tomatoes, and fresh basil come together in a perfect harmony of taste. ",
+    price: "18",
+    image: "",
+    alt: "Image du produit 1",
+    category: "This dish contains crustacean.",
+  },
+  {
+    name: "Thai-style Chicken Wings",
+    content:
+      "Savor the authentic flavors of Thailand with our tender and spicy chicken wings, a delightful blend of sweet and savory. Perfect for sharing!",
+    price: "17",
+    image: "",
+    alt: "Image du produit 2",
+    category: "This dish contains poultry.",
+  },
+];
+
+const liste2 = [
+  {
+    name: "Ricotta and Basil Pasta",
+    content:
+      "Enjoy the goodness of our Ricotta and Basil Pasta, a delightful fusion of creamy ricotta and aromatic basil for a satisfying meal.",
+    price: "32",
+    image: "",
+    alt: "Image du produit 1",
+    category: "Vegetarian.",
+  },
+  {
+    name: "Saffron Butternut Squash Ravioli",
+    content:
+      "Indulge in the rich flavors of our Saffron Butternut Squash Ravioli. A delectable combination of butternut squash and saffron in every bite.",
+    price: "38",
+    image: "",
+    alt: "Image du produit 1",
+    category: "Vegetarian.",
+  },
+];
+
+export { liste, liste2 };
--- src/views/Menupage/Menupage.jsx
+++ src/views/Menupage/Menupage.jsx
@@ -1,11 +1,40 @@
-import Product from "../../compments/Product";
+import { liste, liste2 } from "/src/data/data.js";
+
+const ProductMenus = (props) => {
+  return (
+    <div className="grid grid-cols-3 gap-4">
+      {props.list.map((product, index) => (
+        <Product key={index} {...product} />
+      ))}
+    </div>
+  );
+};
+
+const Product = ({ name, content, price, category, image, alt }) => {
+  return (
+    <div className="card w-96 bg-base-100 shadow-xl">
+      <figure>
+        <img src={image} alt={alt} />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">{name}</h2>
+        <p>{content}</p>
+        <p>{price}</p>
+        <div className="card-actions justify-end">
+          <div className="badge badge-outline">{category}</div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 function Menu() {
   return (
     <div>
       <h1>Menu</h1>
       <section>
-        <Product name="" content="" price="" category="" image="" alt="" />
+        <ProductMenus list={liste} />
+        <ProductMenus list={liste2} />
       </section>
     </div>
   );
