c7dcf825e59460e23bc8eda1f267164a5ec67d00
matthieuGravy <matthieugravy@gmail.com>
2023-11-17 21:42:56 +0100

    hero

---
+++ src/compments/BgHero.jsx
@@ -0,0 +1,31 @@
+function BgHero() {
+  return (
+    <>
+      <div
+        className="hero min-h-screen"
+        style={{
+          backgroundImage: "url(src/assets/client-2.jpg)",
+        }}
+      >
+        <div className="hero-overlay bg-opacity-60"></div>
+        <div className="hero-content text-center text-neutral-content">
+          <div className="max-w-md">
+            <h1 className="mb-5 text-5xl font-bold">
+              Discover It's Gravy Kitchens
+            </h1>
+            <p className="mb-5">
+              An Enchanted Place Where World Flavors Unite to Create an
+              Unforgettable Culinary Experience. Embark on a Culinary World Tour
+              and Let Your Taste Buds Explore the Flavors of the Globe in Our
+              Exquisite Restaurant, Where Every Dish Tells a Different Story of
+              International Cuisine.
+            </p>
+            <button className="btn btn-secondary">Get Started</button>
+          </div>
+        </div>
+      </div>
+      ;
+    </>
+  );
+}
+export { BgHero };
--- src/views/Homepage/Homepage.jsx
+++ src/views/Homepage/Homepage.jsx
@@ -1,22 +1,10 @@
 import { NavLink } from "react-router-dom";
+import { BgHero } from "../../compments/BgHero";
 
 function Homepage() {
   return (
     <>
-      <h1 className="text-6xl pt-5 px-2">Discover It's Gravy Kitchens</h1>
-      <article className="px-2 pt-5">
-        <p>
-          An Enchanted Place Where World Flavors Unite to Create an
-          Unforgettable Culinary Experience. Embark on a Culinary World Tour and
-          Let Your Taste Buds Explore the Flavors of the Globe in Our Exquisite
-          Restaurant, Where Every Dish Tells a Different Story of International
-          Cuisine.
-        </p>
-        <div>
-          <NavLink to="/menu">Menu</NavLink>
-          <NavLink to="/contact">Contact</NavLink>
-        </div>
-      </article>
+      <BgHero />
     </>
   );
 }
